b13c6cc1b3c9a9dea321967a3ef25fb93a1e2757
cisimon7 <cisimon7@gmail.com>
2022-11-04 15:26:30 +0200

    refactor::docs_api_minimization

--- ars/api_minimize.py
+++ ars/api_minimize.py
@@ -7,6 +7,30 @@ from tqdm import tqdm
 from .ARSOptimizer import ARSOptimizer
 
 
+class MinimizationEnv:
+    def __init__(self, params, obj_func):
+        """
+        The minimization problem is treated like an RL problem, but a much simpler problem than typical RL problems
+        :param params: the arguments to be minimized
+        :param obj_func: the objective function of the minimization function
+        """
+        self.params = params
+        self.obj_func = obj_func
+
+    def reset(self):
+        return self.params
+
+    def step(self, action):
+        """
+        The reward function is simply the negative of the objective function at the current values of the minimization
+        arguments
+        :param action: Action isn't needed for the minimization problem
+        :return: A tuple of (next_state, reward, done)
+        """
+        x = self.params
+        return None, - self.obj_func(x).detach().numpy(), True
+
+
 def ars_minimize(
         obj_func: Callable,
         n_vars=1,
@@ -14,21 +38,18 @@ def ars_minimize(
         on_step: Callable[[Tensor, float, int], Any] = lambda goodness: None,
         **ars_opti_kwargs
 ):
-    class MinimizationEnv:
-        def __init__(self, params):
-            self.params = params
-
-        def reset(self):
-            return self.params
-
-        def step(self, action):
-            x = self.params
-            return None, - obj_func(x).detach().numpy(), True
-
+    """
+    Unconstrained Optimization problem formulation using the ARS algorithm
+    :param obj_func: Objective function to be minimized
+    :param n_vars:  number of minimization args
+    :param n_steps: Number of optimization steps to be taken
+    :param on_step: Function to perform on each optimization step given the current minimization values, reward function and step count
+    :return: None
+    """
     xy_t = th.Tensor([0 for _ in range(n_vars)])
     optimizer = ARSOptimizer(
         xy_t,
-        get_env=lambda params: MinimizationEnv(params),
+        get_env=lambda params: MinimizationEnv(params, obj_func),
         action_sz=1,
         get_policy=(lambda params, normalizer: (lambda x: None)),
         sdv=1E-3,
